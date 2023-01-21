edc42230973e6ae4e566f80c5b7b092d0e3fbebc
Miles D. Ackerman <43039831+mdbackerman@users.noreply.github.com>
2023-01-20 20:52:05 -0500

    Final commits in branch

--- Application_folder/Window_Contents.py
+++ Application_folder/Window_Contents.py
@@ -220,8 +220,8 @@ C:/Users/lukin2dmaterials/miniconda3/envs/qcodes/Lib/site-packages/qcodes_contri
             desired_end_y_mirror_voltage = round(float(xy_scan_y_voltage_max_qlineedit.text()), 2)
 
             # def the internal stepping voltages based on user-entered settings above
-            x_drive_voltage_step = ((np.absolute(initial_x_driving_voltage)) + (desired_end_x_mirror_voltage)) / grid_size_x
-            y_drive_voltage_step = ((np.absolute(initial_y_driving_voltage)) + (desired_end_y_mirror_voltage)) / grid_size_y
+            x_drive_voltage_step = round(((np.absolute(initial_x_driving_voltage)) + (desired_end_x_mirror_voltage)) / grid_size_x, 5)
+            y_drive_voltage_step = round(((np.absolute(initial_y_driving_voltage)) + (desired_end_y_mirror_voltage)) / grid_size_y, 5)
 
             # create dataset to populate
             global xy_scan_data_array
@@ -291,7 +291,7 @@ C:/Users/lukin2dmaterials/miniconda3/envs/qcodes/Lib/site-packages/qcodes_contri
 
                         ################## important section #################
 
-                        for my_var_not_named_i in range(int(scan_counter_acquisition_time * 500)): # this reads/lets the counter accumulate for the set time and returns value
+                        for my_var_not_named_i in range(int(scan_counter_acquisition_time * 1000)): # this reads/lets the counter accumulate for the set time and returns value
                             counter_value = task1.read()
                             output_value += counter_value
 
@@ -314,6 +314,9 @@ C:/Users/lukin2dmaterials/miniconda3/envs/qcodes/Lib/site-packages/qcodes_contri
                             if k < (grid_size_x - 1):
 
                                 x_driving_voltage_to_change += x_drive_voltage_step # increment drive voltage forwards
+
+                                x_driving_voltage_to_change = round(x_driving_voltage_to_change, 5)
+
                                 scan_galvo.voltage_cdaq1mod2ao0(x_driving_voltage_to_change) # step x motor
 
                             else:
@@ -322,6 +325,9 @@ C:/Users/lukin2dmaterials/miniconda3/envs/qcodes/Lib/site-packages/qcodes_contri
                             if k < (grid_size_x - 1):
 
                                 x_driving_voltage_to_change -= x_drive_voltage_step # increment drive voltage backwards
+
+                                x_driving_voltage_to_change = round(x_driving_voltage_to_change, 5)
+
                                 scan_galvo.voltage_cdaq1mod2ao0(x_driving_voltage_to_change) # step x motor
 
                             else:
@@ -348,6 +354,9 @@ C:/Users/lukin2dmaterials/miniconda3/envs/qcodes/Lib/site-packages/qcodes_contri
                     if f < (grid_size_y - 1): # this loop prevents from scanning an upper undesired row
 
                         y_driving_voltage_to_change += y_drive_voltage_step # increment drive voltage
+
+                        y_driving_voltage_to_change = round(y_driving_voltage_to_change, 5)
+
                         scan_galvo.voltage_cdaq1mod2ao1(y_driving_voltage_to_change) # step y motor
 
                     else:
