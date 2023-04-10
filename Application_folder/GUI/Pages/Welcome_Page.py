97c45e286fbb3d7204e0c89fc990be7d63f653cc
Miles D. Ackerman <43039831+mdbackerman@users.noreply.github.com>
2023-04-10 15:56:20 -0400

    Refining welcome page (add developed by; worked on positioning)

--- Application_folder/GUI/Pages/Welcome_Page.py
+++ Application_folder/GUI/Pages/Welcome_Page.py
@@ -55,32 +55,51 @@ def build_welcome_page(self): # define build_welcome_page to setup the welcome U
 
     ###################################################################################### start contents #############################################################################################
     y_axis_offset_temp = -75
+
     # welcome widget (QLabel)
-    self.application_welcome_widget = QLabel("Welcome to:", self)
-    self.application_welcome_widget.setParent(self.welcome_background_frame_top)
-    self.application_welcome_widget.setFont(QFont("Times", 12))
-    self.application_welcome_widget.move(397, 62 - y_axis_offset_temp)
+    self.application_welcome_widget = QLabel("Welcome to:", self) # create welcome widget
+
+    self.application_welcome_widget.setParent(self.welcome_background_frame_top) # designate parent of welcome widget to top frame
+
+    self.application_welcome_widget.setFont(QFont("Times", 12)) # set font and font size of welcome widget
+
+    self.application_welcome_widget.move(397, 62 - y_axis_offset_temp) # position the welcome widget
 
     # title widget: (QLabel)
-    self.welcome_title_widget = QLabel("MDA b017 GUI") # create title widget
+    self.welcome_title_widget = QLabel("MDA b017 GUI") # create the title widget (application name)
 
     self.welcome_title_widget.setParent(self.welcome_background_frame_top) # designate parent of title widget
 
     self.welcome_title_widget.setFont(QFont("Amagro", 38)) # set font of welcome page title widget
 
     self.welcome_title_widget.move(280, 80 - y_axis_offset_temp) # position the title
-
+    
     # version widget (QLabel)
-    self.application_version_widget = QLabel("Version:")
-    self.application_version_widget.setParent(self.welcome_background_frame_top)
-    self.application_version_widget.setFont(QFont("Times", 11))
-    self.application_version_widget.move(379 + 17, 136 - y_axis_offset_temp)
+    self.application_version_widget = QLabel("Version:") # create the version QLabel
+
+    self.application_version_widget.setParent(self.welcome_background_frame_top) # designate the parent of the version widget (top window)
+
+    self.application_version_widget.setFont(QFont("Times", 11)) # set the font and font size of the version widget
+
+    self.application_version_widget.move(401, 135 - y_axis_offset_temp) # position the version widget
 
     # version number widget: (QLabel)
-    self.application_version_number_widget = QLabel("0.0")
-    self.application_version_number_widget.setParent(self.welcome_background_frame_top)
-    self.application_version_number_widget.setFont(QFont("Arial", 11))
-    self.application_version_number_widget.move(440 + 10, 138 - y_axis_offset_temp)
+    self.application_version_number_widget = QLabel("0.0") # create a version number widget (QLabel)
+
+    self.application_version_number_widget.setParent(self.welcome_background_frame_top) # designate the parent of the version number widget
+
+    self.application_version_number_widget.setFont(QFont("Arial", 11)) # set the font and font size of the version number widget
+
+    self.application_version_number_widget.move(456, 138 - y_axis_offset_temp) # position the version number widget
+
+    # developed by widget: (QLabel)
+    self.developed_by_widget = QLabel("Developed by: Miles Ackerman") # create a developed by widget (QLabel)
+
+    self.developed_by_widget.setParent(self.welcome_background_frame_top) # designate the parent of the developed by widget
+
+    self.developed_by_widget.setFont(QFont("Arial", 9)) # set the font and font size of the developed by widget
+
+    self.developed_by_widget.move(360, 158 - y_axis_offset_temp) # position the developed by widget
 
     # pre-link GitHub widget (QLabel)
     self.GitHub_link_indication_QLabel = QLabel("Click here to visit the application's:", self)
